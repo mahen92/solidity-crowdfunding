1031cb3a778ffe04c838f5b3c6b90b4030148d10
Mahendran <mahendrananbarasan@gmail.com>
2022-12-26 23:01:30 +0530

    Create Factory.sol

---
+++ Factory.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+import "./CrowdFundBaseContract.sol";
+import "./CFToken.sol";
+contract Factory {
+  event Deployed(address addr);
+  address public dep;
+  function deploy(address tokenAddress) public returns(address) {
+        CrowdFundBaseContract c=new CrowdFundBaseContract(6,tokenAddress); 
+        address newProjectAddress = address(c); // here
+        CFToken(tokenAddress).addContract(newProjectAddress);
+        dep=newProjectAddress;
+
+        return newProjectAddress;
+  }
+}
