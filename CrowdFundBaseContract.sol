7406a0d9cb3c884038ff4400d7642bf0f2c4421a
Mahendran <mahendrananbarasan@gmail.com>
2022-12-27 15:49:49 +0530

    Update CrowdFundBaseContract.sol

--- CrowdFundBaseContract.sol
+++ CrowdFundBaseContract.sol
@@ -1,39 +1,119 @@
 pragma solidity ^0.8.0;
 
-contract CrowdFundBaseContract{
-     mapping(address=>uint256) public addressMapping;
-     mapping(address=>bool) public availabilityMapping;
-     address public immutable cftoken;
-     bool hasSetToken;
-     uint public immutable setBlockNumber=block.number;
-     uint public immutable fundDuration;
-
-
-    constructor(uint256 _fundDuration,address _cftoken){
-       fundDuration=_fundDuration;
-       cftoken=_cftoken;
+import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+import "./CFToken.sol";
+contract Fund is ReentrancyGuardUpgradeable{
+    
+    struct crowdFund{
+        string key;
+        uint goal;
+        address beneficiary;
+        bool isOver;
+        uint opening;
+        uint closing;
+        uint value;
     }
 
-    modifier onlyCfToken {
-        require(msg.sender == cftoken, "Only the assigned CF Token can perform this action");
-        _;
+    uint public number;
+    CFToken public token;
+    uint public duration;
+    mapping(uint => crowdFund) public funds;
+    mapping(uint => mapping(address => uint)) public contributions;
+
+     event Start(
+        uint id,
+        address indexed creator,
+        uint goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+
+    event Cancel(uint id);
+    event Pledge(uint indexed id, address indexed caller, uint amount);
+    event Unpledge(uint indexed id, address indexed caller, uint amount);
+    event Claim(uint id);
+    event Refund(uint id, address indexed caller, uint amount);
+
+     function initialize(address payable _token, uint _duration) 
+  public     {
+      token = CFToken(_token);
+      duration=_duration;
+  }
+
+  function launch(uint _goal, uint32 _startAt, uint32 _endAt) external {
+        require(_startAt >= block.timestamp,"Start time is less than current Block Timestamp");
+        require(_endAt > _startAt,"End time is less than Start time");
+        require(_endAt <= block.timestamp + duration, "End time exceeds the maximum Duration");
+
+        number += 1;
+        funds[number] = crowdFund({
+            beneficiary: msg.sender,
+            goal: _goal,
+            value: 0,
+            opening: _startAt,
+            closing: _endAt,
+            isOver: false,
+            key:"Plus"
+        });
+
+        emit Start(number,msg.sender,_goal,_startAt,_endAt);
     }
 
-    modifier onlyTokenNotSet {
-        require(hasSetToken == false, "The token has already been set");
-        _;
+    function cancel(uint _id) external {
+        crowdFund memory fund = funds[_id];
+        require(fund.beneficiary == msg.sender, "You did not create this Campaign");
+        require(block.timestamp < fund.opening, "Campaign has already started");
+
+        delete funds[_id];
+        emit Cancel(_id);
     }
 
+    function pledge(uint _id, uint _amount) external {
+        crowdFund storage fund = funds[_id];
+        require(block.timestamp >= fund.opening, "Campaign has not Started yet");
+        require(block.timestamp <= fund.closing, "Campaign has already ended");
+        fund.value += _amount;
+        contributions[_id][msg.sender] += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
 
-     function addAmount(address user,uint256 amount) public onlyCfToken{
-         if(!availabilityMapping[user])
-         {
-            availabilityMapping[user]=true;
-            addressMapping[user]=amount;
-         }
-         else
-         {
-            addressMapping[user]=addressMapping[user]+amount;
-         }
-     }
+        emit Pledge(_id, msg.sender, _amount);
+    }
+
+    function unPledge(uint _id,uint _amount) external {
+        crowdFund storage fund = funds[_id];
+        require(block.timestamp >= fund.opening, "Campaign has not Started yet");
+        require(block.timestamp <= fund.closing, "Campaign has already ended");
+        require(contributions[_id][msg.sender] >= _amount,"You do not have enough tokens Pledged to withraw");
+
+        fund.value -= _amount;
+        contributions[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit Unpledge(_id, msg.sender, _amount);
+    }
+
+    function claim(uint _id) external {
+        crowdFund storage fund = funds[_id];
+        require(fund.beneficiary == msg.sender, "You did not create this Campaign");
+        require(block.timestamp > fund.closing, "Campaign has not ended");
+        require(fund.value >= fund.goal, "Campaign did not succed");
+        require(!fund.isOver, "claimed");
+
+        fund.isOver = true;
+        token.transfer(fund.beneficiary, fund.value);
+
+        emit Claim(_id);
+    }
+
+    function refund(uint _id) external {
+        crowdFund memory fund = funds[_id];
+        require(block.timestamp > fund.closing, "not ended");
+        require(fund.value < fund.goal, "You cannot Withdraw, Campaign has succeeded");
+
+        uint bal = contributions[_id][msg.sender];
+        contributions[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Refund(_id, msg.sender, bal);
+    }
 }
