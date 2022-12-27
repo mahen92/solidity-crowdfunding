f0df81926c9393069e98e708208e2d888aafe4d3
Mahendran <mahendrananbarasan@gmail.com>
2022-12-27 17:49:50 +0530

    Update CrowdFundBaseContract.sol

--- CrowdFundBaseContract.sol
+++ CrowdFundBaseContract.sol
@@ -4,7 +4,7 @@ import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 import "./CFToken.sol";
 contract Fund is ReentrancyGuardUpgradeable{
     
-    string key;
+    uint256 key;
     uint goal;
     address beneficiary;
     bool isOver;
@@ -12,92 +12,101 @@ contract Fund is ReentrancyGuardUpgradeable{
     uint closing;
     uint value;
     
-
-    
     bool cancelled;
     uint public number;
     CFToken public token;
     uint public duration;
     mapping(address => uint) public contributions;
 
-     event Start(
-        uint id,
-        address indexed creator,
-        uint goal,
-        uint32 startAt,
-        uint32 endAt
-    );
 
-    event Cancel(uint id);
-    event Pledge(uint indexed id, address indexed caller, uint amount);
-    event Unpledge(uint indexed id, address indexed caller, uint amount);
-    event Claim(uint id);
-    event Refund(uint id, address indexed caller, uint amount);
+    event Cancel(uint256 indexed key);
+    event Pledge(uint256 indexed key, address indexed caller, uint amount);
+    event Unpledge(uint256 indexed key, address indexed caller, uint amount);
+    event Claim(uint256 indexed key);
+    event Refund(uint256 indexed key, address indexed caller, uint amount);
+
+    error FundIsNotCancelled();
+    error FundHasSucceeded();
+    error FundHasNotEnded();
 
      function initialize(address payable _token, uint _duration,uint _goal,
-     address _beneficiary,uint _opening,uint _closing,uint _value) 
+     address _beneficiary,uint _opening,uint _closing,uint _value,uint256 _number) 
   public     {
-      token = CFToken(_token);
-      duration=_duration;
+     token = CFToken(_token);
+     duration=_duration;
      goal=_goal;
      beneficiary=_beneficiary;
      opening=_opening;
      closing=_closing;
      value=_value;
+     key=number;
   }
 
-  
-
-    function cancel(uint _id) external {
-        require(beneficiary == msg.sender, "You did not create this Campaign");
-        require(block.timestamp < opening, "Campaign has already started");
-
-        cancelled=true;
-        emit Cancel(_id);
+     modifier onlyBeneficiary {
+        require(msg.sender == beneficiary, "Only the beneficiary can perform this action.");
+        _;
     }
 
-    function pledge(uint _id, uint _amount) external {
-        require(block.timestamp >= opening, "Campaign has not Started yet");
-        require(block.timestamp <= closing, "Campaign has already ended");
+    modifier onlyActiveFund {
+        require(cancelled == false, "This fund has been cancelled");
+        require(block.timestamp >= opening, "Funding is yet to begin");
+        require(block.timestamp <= closing, "Funding has ended");
+
+        _;
+    }
+
+    modifier onlyValidClaim {
+        require(value >= goal, "Campaign did not succed");
+        require(!isOver, "claimed");
+
+        _;
+    }
+
+    function cancel() external onlyBeneficiary{
+        cancelled=true;
+        emit Cancel(key);
+    }
+
+    function pledge(uint _amount) external onlyActiveFund  {
         value += _amount;
         contributions[msg.sender] += _amount;
         token.transferFrom(msg.sender, address(this), _amount);
 
-        emit Pledge(_id, msg.sender, _amount);
+        emit Pledge(key, msg.sender, _amount);
     }
 
-    function unPledge(uint _id,uint _amount) external {
-        require(block.timestamp >= opening, "Campaign has not Started yet");
-        require(block.timestamp <= closing, "Campaign has already ended");
-        require(contributions[msg.sender] >= _amount,"You do not have enough tokens Pledged to withraw");
-
-        value -= _amount;
-        contributions[msg.sender] -= _amount;
-        token.transfer(msg.sender, _amount);
-
-        emit Unpledge(_id, msg.sender, _amount);
-    }
-
-    function claim(uint _id) external {
-        require(beneficiary == msg.sender, "You did not create this Campaign");
-        require(block.timestamp > closing, "Campaign has not ended");
-        require(value >= goal, "Campaign did not succed");
-        require(!isOver, "claimed");
+    function claim() public onlyBeneficiary onlyValidClaim {
 
         isOver = true;
         token.transfer(beneficiary, value);
 
-        emit Claim(_id);
+        emit Claim(number);
     }
 
-    function refund(uint _id) external {
-        require(block.timestamp > closing, "not ended");
-        require(value < goal, "You cannot Withdraw, Campaign has succeeded");
+    function refund() external  {
+        if(value>goal)
+        {
+          revert FundHasSucceeded();
+        }
+        if(block.timestamp <= closing)
+        {
+         revert FundHasNotEnded();
+        }
 
         uint bal = contributions[msg.sender];
         contributions[msg.sender] = 0;
         token.transfer(msg.sender, bal);
 
-        emit Refund(_id, msg.sender, bal);
+        emit Refund(key,msg.sender, bal);
+    }
+
+    function withdraw() external {
+        if(cancelled!=true)
+        {
+        revert FundIsNotCancelled();
+        }
+        uint bal = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+        token.transfer(msg.sender, bal);
     }
 }
